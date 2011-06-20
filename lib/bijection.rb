a0462f64bdd92b3daafd91afba8d13b3ccab6f89
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-06-20 02:50:55 -0400

    bijection.base: added prelim api and docs for delete_x, delete_y, domain, each_pair, get_x, get_y, inverse!, map, and range; updated .gitignore

--- lib/bijection.rb
+++ lib/bijection.rb
@@ -1,8 +1,10 @@
 require "bijection/version"
 require "bijection/base"
 
+# @example b = Bijection.new
 module Bijection
   class << self
+    # @return [Base]
     def new
       Bijection::Base.new
     end

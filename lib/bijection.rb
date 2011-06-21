a9f13b6805ff8834b330976156d9dce09052a3d2
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-06-21 01:02:27 -0400

    rename bijection.add from bijection.map; spec examples, implement api

--- lib/bijection.rb
+++ lib/bijection.rb
@@ -1,50 +1,73 @@
 
+# Bijection is a container similar to a Hash with unique values.
+# {http://en.wikipedia.org/wiki/Bijection}
+#
 # @example b = Bijection.new
 class Bijection
   VERSION = "0.0.1"
 
-  # @private
   def initialize
-    @x = {}
-    @y = {}
+    @X = {} # @X[x] -> y
+    @Y = {} # @Y[y] -> x
+  end
+
+  # returns the number of pairs in the bijection
+  # @return [Fixnum]
+  def size
+    @X.size
   end
 
   # add (x,y) to the bijection set (X,Y)
-  # @param x the object associated with y
-  # @param y the object associated with x
+  # @param x the non-nil object associated with y
+  # @param y the non-nil object associated with x
   # @note x must be unique in X; y must be unique in Y
   # @return self
-  def map( x, y )
+  def add( x, y )
+    raise "Bijection: x may not be nil" if x == nil
+    raise "Bijection: y may not be nil" if y == nil
+    raise "Bijection: x:#{x.to_s} already present in X" if @X.key? x
+    raise "Bijection: y:#{y.to_s} already present in Y" if @Y.key? y
+    @X[x] = y
+    @Y[y] = x
     self
   end
 
   # @return [Enumerator] the domain set X
   def domain
-    @x.each_key
+    @X.each_key
   end
 
   # @return [Enumerator] the range set Y
   def range
-    @y.each_key
+    @Y.each_key
   end
 
   # swap domain X and range Y of this
   # @return nil
   def inverse!
+    temp = @X
+    @X = @Y
+    @Y = temp
+    nil
   end
 
+  # get the x associated with y
   # @param y in domain set Y
-  # @return the x associated with y
+  # @return the x associated with y, or nil if y not in range Y
   def get_x( y )
+    @Y[y]
   end
 
+  # get the y associated with x
   # @param x in domain set X
-  # @return the y associated with x
+  # @return the y associated with x, or nil if x not in domain X
   def get_y( x )
+    @X[x]
   end
 
   # @return [Enumerator] an enumerator all (x,y)
   def each_pair
+    @X.each_pair
   end
 
   # given x, delete (x,y) and return y
@@ -52,16 +75,24 @@ class Bijection
   # @example
   #  b = Bijection.new
   #  x = 2 ; y = 3
-  #  b.map x, y
+  #  b.add x, y
   #  y = nil
-  #  while 1 { y = b.delete_x x ; b.map x, y }
-  # @return the y associated with x
-  def delete_x( x )
+  #  while 1 { y = b.delete_x x ; b.add x, y }
+  # @return (see #get_x)
+  def delete_by_x( x )
+    y = @X[x]
+    @X.delete x
+    @Y.delete y
+    y
   end
 
   # given y, delete (x,y) and return x
   # @param (see #get_y)
-  # @return x associated with y
-  def delete_y( y )
+  # @return (see #get_y)
+  def delete_by_y( y )
+    x = @Y[y]
+    @Y.delete y
+    @X.delete x
+    x
   end
 end
--- spec/lib/bijection_spec.rb
+++ spec/lib/bijection_spec.rb
@@ -1,12 +1,140 @@
 require "spec_helper"
 
 describe Bijection do
-  subject { Bijection.new }
+  before(:each) { @b = Bijection.new }
 
-  it "initializes empty domain" do
-    subject.domain.size.should == 0
+  it "initializes with empty domain" do
+    i = 0
+    @b.domain.each { i += 1 }
+    i.should == 0
   end
 
-  pending "blah"
+  it "initializes with empty range" do
+    i = 0
+    @b.range.each { i += 1 }
+    i.should == 0
+  end
+
+  it "initializes with empty size" do
+    @b.size.should == 0
+  end
+
+  it "returns self when add is called" do
+    @b.add(5, 3).should == @b
+  end
+
+  it "returns nil when inverse! is called" do
+    @b.inverse!.should be_nil
+  end
+
+  it "add raises when x is nil" do
+    expect { @b.add nil, "fooz" }.to raise_error
+  end
+
+  it "add raises when y is nil" do
+    expect { @b.add 57, nil }.to raise_error
+  end
+
+  it "delete_by_x returns nil for x not in X" do
+    @b.delete_by_x(5).should be_nil
+  end
+
+  it "delete_by_y returns nil for y not in Y" do
+    @b.delete_by_y(5).should be_nil
+  end
+
+  context "with one pair added" do
+    before :each do
+      @x = 3
+      @y = []
+      @b.add @x, @y
+    end
+
+    it "finds x in the domain" do
+      @b.domain.peek.should == @x
+    end
+
+    it "finds y in the range" do
+      @b.range.peek.should == @y
+    end
+
+    it "finds x,y in each_pair" do
+      @b.each_pair do |x,y|
+        x.should == @x
+        y.should == @y
+      end
+    end
+
+    it "raises when x is re-added" do
+      expect { @b.add @x, "fooz" }.to raise_error
+    end
+
+    it "raises when y is re-added" do
+      expect { @b.add "sdfdsfs", @y }.to raise_error
+    end
+
+    it "returns x when given y" do
+      @b.get_x(@y).should == @x
+    end
+
+    it "returns y when given x" do
+      @b.get_y(@x).should == @y
+    end
+
+    it "returns nil when given x not in X" do
+      @b.get_y(7).should be_nil
+    end
+
+    it "returns nil when given y not in Y" do
+      @b.get_x("foo").should be_nil
+    end
+
+    it "returns x for delete_by_y y and then the bijection is empty" do
+      @b.delete_by_y(@y).should == @x
+      @b.size == 0
+    end
+
+    it "returns y for delete_by_x x and then the bijection is empty" do
+      @b.delete_by_x(@x).should == @y
+      @b.size == 0
+    end
+
+    context "after deleting (x,y)" do
+      before :each do
+        @b.delete_by_x @x
+      end
+
+      it "bijection is empty" do
+        @b.size.should == 0
+      end
+
+      it "allows re-adding of (x,y)" do
+        @b.add @x, @y
+      end
+
+      context "when re-adding (x,y)" do
+        before :each do
+          @b.add @x, @y
+        end
+
+        it "should have size 1" do
+          @b.size.should == 1
+        end
+      end
+    end
+
+    context "the inverse" do
+      before(:each) do
+        @b.inverse!
+      end
+
+      it "returns y for get_x" do
+        @b.get_x(@x).should == @y
+      end
+
+      it "returns x for get_y" do
+        @b.get_y(@y).should == @x
+      end
+    end # context inverse
+  end # context one pair added
 end
-# stub
